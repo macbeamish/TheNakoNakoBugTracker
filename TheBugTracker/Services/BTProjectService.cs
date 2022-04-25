1ff2c179bd48e13af1918d71afa56d023ccb9569
macbeamish <75453707+macbeamish@users.noreply.github.com>
2022-04-25 14:58:09 +1200

    project service methods partially added

--- TheBugTracker/Services/BTProjectService.cs
+++ TheBugTracker/Services/BTProjectService.cs
@@ -15,7 +15,7 @@ namespace TheBugTracker.Services
             _context = context;
 
         }
-
+        //CRUD -- Create
         public async Task AddNewProjectAsync(Project project)
         {
             _context.Add(project);
@@ -29,12 +29,39 @@ namespace TheBugTracker.Services
 
         public async Task<bool> AddUserToProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+          BTUser user = await _context.Users.FirstOrDefaultAsync(u=>u.Id == userId);
+            if (user == null)
+            {
+                Project  project = await _context.Projects.FirstOrDefaultAsync(p=>p.Id == projectId);
+                if(!await IsUserOnProjectAsync(userId, projectId))
+                {
+                    try
+                    {
+                        project.Members.Add(user);
+                        await _context.SaveChangesAsync();
+                        return true;    
+                    }
+                    catch(Exception)
+                    {
+                        throw;
+                    }       
+                }
+                else
+                {
+                    return false;   
+                }
+            }
+            else
+            {
+                return false;
+            }            
         }
-
+        //CRUD -- Archive (Delete)
         public async Task ArchiveProjectAsync(Project project)
         {
-            throw new NotImplementedException();
+            project.Archived = true;    
+           _context.Update(project);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<List<BTUser>> GetAllProjectMembersExceptPMAsync(int projectId)
@@ -44,27 +71,56 @@ namespace TheBugTracker.Services
 
         public async Task<List<Project>> GetAllProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            List<Project> projects = new();
+            projects = await _context.Projects.Where(p => p.CompanyID == companyId && p.Archived == false)
+                                                .Include(p => p.Members)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.Comments)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketPriority)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketStatus)
+                                                  .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketType)
+                                                 .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.Attachments)
+                                                 .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.OwnerUser)
+                                                 .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.DeveloperUser)
+                                                 .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.History)
+                                                .Include(p => p.ProjectPriority)
+                                                .ToListAsync();
+            return projects;    
         }
-
+        // CRUD -- Read projects by company and priority
         public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
         {
-            throw new NotImplementedException();
+            List<Project> projects = await GetAllProjectsByCompany(companyId);
+            int priorityId = await LookupProjectPriorityId(priorityName);
+            return projects.Where(p => p.ProjectPriorityId == priorityId).ToList();
         }
-
+        //CRUD -- Read archived projects by company
         public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            List<Project> projects = await GetAllProjectsByCompany(companyId);
+            return projects.Where(p => p.Archived == true).ToList();
         }
 
         public async Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
         {
             throw new NotImplementedException();
         }
-
+        //CRUD -- Read
         public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
-            throw new NotImplementedException();
+            Project project = await _context.Projects
+                                    .Include(p => p.Tickets)
+                                    .Include(p => p.Members)
+                                    .Include(p => p.ProjectPriority)
+                                    .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyID == companyId);
+            return project;
         }
 
         public async Task<BTUser> GetProjectManagerAsync(int projectId)
@@ -84,7 +140,37 @@ namespace TheBugTracker.Services
 
         public async Task<List<Project>> GetUserProjectsAsync(string userId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Project> userProjects = (await _context.Users
+                                            .Include(u=>u.Projects)
+                                                .ThenInclude(p=>p.Company)
+                                            .Include(u => u.Projects)
+                                                .ThenInclude(p => p.Members)
+                                            .Include(u => u.Projects)
+                                                .ThenInclude(t => t.Tickets)
+                                                    .ThenInclude(t => t.DeveloperUser)
+                                             .Include(u => u.Projects)
+                                                .ThenInclude(t => t.Tickets)
+                                                    .ThenInclude(t => t.OwnerUser)
+                                             .Include(u => u.Projects)
+                                                .ThenInclude(t => t.Tickets)
+                                                    .ThenInclude(t => t.TicketPriority)
+                                             .Include(u => u.Projects)
+                                                .ThenInclude(t => t.Tickets)
+                                                    .ThenInclude(t => t.TicketStatus)
+                                             .Include(u => u.Projects)
+                                                .ThenInclude(t => t.Tickets)
+                                                    .ThenInclude(t => t.TicketType)
+                                            .FirstOrDefaultAsync(u=>u.Id == userId)).Projects.ToList();
+
+                return userProjects;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"******ERROR ***** Error getting user projects. -----> {ex.Message}");
+                throw;
+            }
         }
 
         public async Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
@@ -92,14 +178,21 @@ namespace TheBugTracker.Services
             throw new NotImplementedException();
         }
 
-        public async Task<bool> IsUserOnProject(string userId, int projectId)
+        public async Task<bool> IsUserOnProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            Project project = await _context.Projects.Include(p => p.Members).FirstOrDefaultAsync(p => p.Id == projectId);
+            bool result = false;
+            if (project != null)
+            {
+                result = project.Members.Any(m => m.Id == userId);
+            }
+            return result;
         }
 
         public async Task<int> LookupProjectPriorityId(string priorityName)
         {
-            throw new NotImplementedException();
+            int priorityId = (await _context.ProjectPriorities.FirstOrDefaultAsync(p => p.Name == priorityName)).Id;
+            return priorityId;
         }
 
         public async Task RemoveProjectManagerAsync(int projectId)
@@ -109,17 +202,39 @@ namespace TheBugTracker.Services
 
         public async Task RemoveUserFromProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                BTUser user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+                Project project = await _context.Projects.FirstOrDefaultAsync(p => p.Id == projectId);
+                try
+                {
+                    if (await IsUserOnProjectAsync(userId, projectId))
+                    {
+                        project.Members.Remove(user);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }               
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"******ERROR ***** Error Removing User from Project. -----> {ex.Message}");
+            }
+
         }
 
         public async Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
         {
             throw new NotImplementedException();
         }
-
+        //CRUD -- Update
         public async Task UpdateProjectAsync(Project project)
         {
-            throw new NotImplementedException();
+            _context.Update(project);
+            await _context.SaveChangesAsync();
         }
     }
 }
