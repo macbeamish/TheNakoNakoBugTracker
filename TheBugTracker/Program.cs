be7f4849a7e0584f640f40a6b6ed0c342dbab7a2
macbeamish <75453707+macbeamish@users.noreply.github.com>
2022-04-30 13:43:08 +1200

    Extension, Factory, Claims

--- TheBugTracker/Program.cs
+++ TheBugTracker/Program.cs
@@ -5,25 +5,24 @@ using System.Configuration;
 using TheBugTracker.Data;
 using TheBugTracker.Models;
 using TheBugTracker.Services;
+using TheBugTracker.Services.Factories;
 using TheBugTracker.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration),o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<BTUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddClaimsPrincipalFactory<BTUserClaimsPrincipalFactory>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
-
-builder.Services.AddControllersWithViews();
 
 //Custom Services
 builder.Services.AddScoped<IBTRolesService, BTRolesService>();
@@ -32,10 +31,22 @@ builder.Services.AddScoped<IBTProjectService, BTProjectService>();
 builder.Services.AddScoped<IBTTicketService, BTTicketService>();
 builder.Services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
 builder.Services.AddScoped<IBTNotificationService, BTNotificationService>();
+builder.Services.AddScoped<IBTInviteService, BTInviteService>();
+builder.Services.AddScoped<IBTFileService, BTFileService > ();
 //Custom Email Service using IEmailSender Interface
 builder.Services.AddScoped<IEmailSender, BTEmailService>();
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
-var app = builder.Build();
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+
+//allow legacy date time behaviour
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+//Not sure if this is right
+await DataUtility.ManageDataAsync(app, builder.Configuration);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
